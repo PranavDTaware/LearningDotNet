d06eadaaed1855e4b9a22b22426087278b7431df
PranavT20 <pranavtaware2001@gmail.com>
2024-11-11 01:06:58 +0530

    practice

--- Day_1/HelloApp/Program.cs
+++ Day_1/HelloApp/Program.cs
@@ -5,20 +5,52 @@ using Maths;
 
 Console.WriteLine("Hello, World!");
 
+
+var p = new{firstname = "Pranav" , lastname = "Taware"};
+Console.WriteLine(p.firstname + " " + p.lastname);
+
+
+int count = 10;
+count++;
+
+string company = "Transflower";
+
+
+Console.WriteLine("Count = " +count);
+Console.WriteLine(company);
+
+
+// Account
 Account acc1 = new Account(50000);
 acc1.Deposit(10000);
-
-float currentbanlce1 = acc1.GetBalance();
-Console.WriteLine(currentbanlce1);
+Console.WriteLine("amount deposited = "  + acc1.GetBalance());
 
 acc1.Withdraw(5000);
-Console.WriteLine(acc1.GetBalance());
+Console.WriteLine("Amount after withdrawal = " + acc1.GetBalance());
+
+float currentbalance1 = acc1.GetBalance();  // Get the current balance and store it in a variable
+Console.WriteLine("Final Balance = " + currentbalance1);
 
 
-Complex c1 = new Complex(60,56);
+Account acc2 = new Account();
+Console.WriteLine(acc2.GetBalance());
+
+List<Account> accounts = new List<Account>();
+accounts.Add(acc1);
+accounts.Add(acc2);
+
+foreach(Account theAccount in accounts)
+    {
+        float result = theAccount.GetBalance();
+        Console.WriteLine("Current Balance = {0}", result);
+    }
+
+
+// Complex
+Complex c1 = new Complex(60,50);
 Complex c2 = new Complex(20,40);
-
 
 Complex add = c1+c2;
 
 Console.WriteLine(add.Real);
+Console.WriteLine(add.Imag);
--- Day_1/HelloApp/obj/Debug/net8.0/HelloApp.AssemblyInfo.cs
+++ Day_1/HelloApp/obj/Debug/net8.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d35a9eb1aa60be98f04d2e257e952ec80c81cce8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5332d43c96ab4c5ef4049e0d4e7f0a189efc6ad9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
