abde705b30db080fa41bdb33408c00e55bc5cc54
PranavT20 <pranavtaware2001@gmail.com>
2024-12-12 23:07:36 +0530

    changes

--- Solution 1/DAL/ICatalogDBManager.cs
+++ Solution 1/DAL/ICatalogDBManager.cs
@@ -1,11 +1,5 @@
 ﻿using Catalog;
-using Microsoft.Data.SqlClient;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DAL
 {
