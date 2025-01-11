027cb041a260fe9054be5e3345c1e73a44fc2155
Pranav Taware <pranavtaware2001@gmail.com>
2025-01-11 21:41:09 +0530

    threading

---
+++ Threading/SimpleConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+
+namespace SimpleConsoleApp
+{
+    //Multithreaded Threaded Application
+
+    class Program
+    {
+        // need to execute  both functions is independent manner
+        static void StoreData()
+        {
+            Thread theTread=Thread.CurrentThread;
+            Console.WriteLine("StoreData fn Thread =" + theTread.ManagedThreadId);
+            Console.WriteLine("Storing data into JSON file "); 
+            Thread.Sleep(3000);  
+            Console.WriteLine("Data is successfully writte in file JSON "); 
+        }
+        static void GetRemoteData()
+        {
+            Thread theTread=Thread.CurrentThread;
+            Console.WriteLine("GetRemoteData fn Thread =" + theTread.ManagedThreadId);
+  
+            Console.WriteLine("Getting users data from git repository");
+            Thread.Sleep(4000);
+            //access data from external applications using  RPC
+             Console.WriteLine("Data is received from Remote Server");
+        }
+
+        static void Main(string[] args)
+        {
+            // Primary Thread
+            Thread theTread=Thread.CurrentThread;
+            Console.WriteLine("Main function invoker =" + theTread.ManagedThreadId);
+            Thread.Sleep(5000);
+            Display();
+            //Creating two secondary Threaded using .NET sdk
+            ThreadStart thDelegate1=new ThreadStart(StoreData);
+            Thread thread2=new Thread(thDelegate1);
+            ThreadStart thDelegate2=new ThreadStart(GetRemoteData);
+            Thread thread3=new Thread(thDelegate2);
+            thread2.Start();
+            thread3.Start();
+            Console.WriteLine("Main function is about to be terminated......");
+        }
+        public static void Display()
+        {
+                 Thread theTread=Thread.CurrentThread;
+                 Console.WriteLine("Display fn Thread =" + theTread.ManagedThreadId);
+                 Console.WriteLine("Displaying product catalog data");
+        }
+    }
+}
