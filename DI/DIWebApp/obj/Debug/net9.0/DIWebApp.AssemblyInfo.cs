3bd00d9647a1a79ac3d2bdab09b2d77058effaf0
Pranav Taware <pranavtaware2001@gmail.com>
2025-01-18 15:39:45 +0530

    changes

--- DI/DIWebApp/obj/Debug/net9.0/DIWebApp.AssemblyInfo.cs
+++ DI/DIWebApp/obj/Debug/net9.0/DIWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DIWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e421e1b4ef06e296eaa57ed24e9d005e4914d651")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f7a99724cac303f1213d16a59a1c63bd6266512")]
 [assembly: System.Reflection.AssemblyProductAttribute("DIWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DIWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
