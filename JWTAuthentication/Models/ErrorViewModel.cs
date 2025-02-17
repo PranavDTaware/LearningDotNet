03ea4e80ee0b38925caa6fdccd4209172b812923
Pranav Taware <pranavtaware2001@gmail.com>
2025-02-17 23:42:49 +0530

    JWT

---
+++ JWTAuthentication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JWTAuthentication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
