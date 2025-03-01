198facd04c4f133f9192c3e98ee8f4617944fd61
Pranav Taware <pranavtaware2001@gmail.com>
2025-03-01 23:56:34 +0530

    changes

--- JWTAuthentication/obj/Debug/net9.0/JWTAuthentication.AssemblyInfo.cs
+++ JWTAuthentication/obj/Debug/net9.0/JWTAuthentication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWTAuthentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+442489511a2316f1db3d92fba3c577988e4b1bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205ab3aa2c6f45a9ead48bec8aa87f43aa66e093")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWTAuthentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JWTAuthentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
