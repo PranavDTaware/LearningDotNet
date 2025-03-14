1a8151bee43a942abd2ca8fa55d2c51e63895c21
Pranav Taware <pranavtaware2001@gmail.com>
2025-03-15 01:14:55 +0530

    changes

--- JWTAuthenticationDatabaseAccess/DBContext/JWAContext.cs
+++ JWTAuthenticationDatabaseAccess/DBContext/JWAContext.cs
@@ -8,16 +8,17 @@ public class JWAContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Role> Roles { get; set; }
 
+
     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     // {
     //     string conString = "server=localhost;user=root;database=HR;password='Senetor@2001'";
     //     optionsBuilder.UseMySql(conString, ServerVersion.AutoDetect(conString));
     // }
 
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
         modelBuilder.Entity<User>()
             .HasOne(u => u.Role)                // User has one Role
             .WithMany(r => r.Users)        // Role has many Users
